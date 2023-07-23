7bed814294bf801a124ce89e081b81afb7831f3b
yapyap <wonwoo8@kangwon.ac.kr>
2023-07-23 18:14:51 +0900

    test

--- src/main/java/com/example/stravarefactoring/Service/LocationQueue.java
+++ src/main/java/com/example/stravarefactoring/Service/LocationQueue.java
@@ -101,7 +101,7 @@ public class LocationQueue {
     }
 
 
-    @Scheduled(cron = "0 15 17 * * ?")
+    @Scheduled(cron = "0 17 18 * * ?")
     public void isTime(){
         log.info("is time man!");
         log.info("is time man!");
